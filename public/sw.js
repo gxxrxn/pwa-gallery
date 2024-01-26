f17ac2c366db029287a5d3e67b642a934af32b30
gxxrxn <rbfks1221@naver.com>
2024-01-26 16:33:41 +0900

    feat: manifest 추가

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const _version = "v0";
+const _version = "v0.0.1";
 const cacheName = "pwa-gallery-cache_" + _version;
 
 const log = (...msg) =>
   console.log(`[Fryfolio ServiceWorker ${_version}]`, ...msg);
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   log("service worker installed");
   self.skipWaiting();
 });
