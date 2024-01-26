ac8245204ebf08be03b3de7345b2c3f1c48f0912
gxxrxn <rbfks1221@naver.com>
2024-01-26 16:40:47 +0900

    feat: video input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import VideoInput from "@/components/upload/VideoInput";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-start justify-between p-24">
       <h1 className=" text-2xl font-bold">
         Playground For Developing Gallery in PWA
       </h1>
+      <VideoInput />
     </main>
   );
 }
